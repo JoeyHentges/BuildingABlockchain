ccb36a27e974397b7b73d6af713a9814221bf70b
Joseph Hentges <joeypeterhentges@gmail.com>
2020-03-22 01:37:06 -0500

    Added a basic contract system to the blockchain

--- src/main.js
+++ src/main.js
@@ -16,13 +16,39 @@ const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
 tx1.signTransaction(myKey);
 coin.addTransaction(tx1);
 
-console.log('starting a minier');
+const tx2 = new Transaction(
+  myWalletAddress,
+  'public key goes here',
+  0,
+  class ContractTest {
+    constructor() {
+      this.greeting = 'Hello world!';
+    }
+    setGreeting(greeting) {
+      this.greeting = greeting;
+    }
+    getGreeting() {
+      return this.greeting;
+    }
+  }
+);
+tx2.signTransaction(myKey);
+coin.addTransaction(tx2);
+
 console.log('starting a minier');
 coin.minePendingTransactions(myWalletAddress);
 console.log(
   'balance of miner-address is: ' + coin.getBalanceOfAddress(myWalletAddress)
 );
 
-coin.chain[1].transactions[0].amount = 1;
-
 console.log('Is chain valid? ' + coin.isChainValid());
+
+console.log(JSON.stringify(coin, null, 4));
+
+const code = coin.chain[1].transactions[1].contractCode;
+console.log(code);
+
+const temp = new code();
+console.log(temp.getGreeting());
+console.log(temp.setGreeting('Hey Joey!'));
+console.log(temp.getGreeting());
